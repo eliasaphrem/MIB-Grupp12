5c3d120a51f8222815e21d30e94ef96d6daf0484
eliasaphrem <elias_kaka@hotmail.com>
2023-05-19 10:30:08 +0200

    ändring

--- src/mib/grupp12/Startsida.java
+++ src/mib/grupp12/Startsida.java
@@ -40,6 +40,7 @@ public class Startsida extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -74,6 +75,13 @@ public class Startsida extends javax.swing.JFrame {
             .addGap(0, 26, Short.MAX_VALUE)
         );
 
+        jButton3.setText("Admin");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -81,6 +89,8 @@ public class Startsida extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(99, 99, 99)
                 .addComponent(jButton1)
+                .addGap(82, 82, 82)
+                .addComponent(jButton3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton2)
                 .addGap(76, 76, 76))
@@ -105,7 +115,9 @@ public class Startsida extends javax.swing.JFrame {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButton2)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton2)
+                            .addComponent(jButton3))))
                 .addGap(46, 46, 46))
         );
 
@@ -114,32 +126,39 @@ public class Startsida extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        mittfönster nytt;
-         try {
-             nytt = new mittfönster();
-             nytt.setVisible(true);
-             dispose();
-         } catch (InfException ex) {
-             Logger.getLogger(Startsida.class.getName()).log(Level.SEVERE, null, ex);
-         }
-              
-        
+
+        try {
+            mittfönster nytt = new mittfönster();
+            nytt.setVisible(true);
+            dispose();
+        } catch (InfException ex) {
+            Logger.getLogger(Startsida.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-      ailenlogin nytt;
-         try {
-             nytt = new ailenlogin();
-             nytt.setVisible(true);
-             dispose();
-         } catch (InfException ex) {
-             Logger.getLogger(Startsida.class.getName()).log(Level.SEVERE, null, ex);
-         }
-      
-      
-       
+
+        try {
+            ailenlogin nytt = new ailenlogin();
+            nytt.setVisible(true);
+            dispose();
+        } catch (InfException ex) {
+            Logger.getLogger(Startsida.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+         // TODO add your handling code here:
+         adminlogin nytt = new adminlogin();
+         nytt.setVisible(true);
+         dispose();
+        
+        
+        
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -149,6 +168,7 @@ public class Startsida extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
