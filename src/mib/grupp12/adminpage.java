e157b99624298b666f636692a1249e2397663e4d
eliasaphrem <elias_kaka@hotmail.com>
2023-05-23 22:55:54 +0200

    gg

--- src/mib/grupp12/adminpage.java
+++ src/mib/grupp12/adminpage.java
@@ -42,6 +42,8 @@ public class adminpage extends javax.swing.JFrame {
         raderaalien = new javax.swing.JButton();
         txtagent = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        txtutrustning = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -59,6 +61,13 @@ public class adminpage extends javax.swing.JFrame {
             }
         });
 
+        jButton2.setText("DELETE UTRUSTNING");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -73,11 +82,23 @@ public class adminpage extends javax.swing.JFrame {
                     .addComponent(txtagent, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
                 .addGap(50, 50, 50))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton2)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(29, 29, 29)
+                        .addComponent(txtutrustning, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(178, Short.MAX_VALUE)
+                .addGap(23, 23, 23)
+                .addComponent(txtutrustning, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addComponent(jButton2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 78, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtalien, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtagent, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -146,6 +167,32 @@ public class adminpage extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        String utrustning = txtutrustning.getText();
+        if (utrustning != null) {
+            try {
+
+                String hamta = idb.fetchSingle("SELECT * FROM utrustning WHERE Utrustnings_ID=" + utrustning);
+                if (hamta != null) {
+                    
+                    idb.delete("DELETE FROM innehar_utrustning WHERE Utrustnings_ID=" + utrustning);
+                    idb.delete("DELETE FROM kommunikation WHERE Utrustnings_ID=" + utrustning);
+                    idb.delete("DELETE FROM teknik WHERE Utrustnings_ID=" + utrustning);
+                    idb.delete("DELETE FROM vapen WHERE Utrustnings_ID=" + utrustning);
+                    idb.delete("DELETE FROM utrustning WHERE Utrustnings_ID=" + utrustning);
+                    JOptionPane.showMessageDialog(rootPane, "Utrustning är nu raderad");
+                }
+                else{
+                JOptionPane.showMessageDialog(rootPane, "Finns ingen utrustning med det namnet");
+                }
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(rootPane, "Vänligen skriv in utrustning");
+            }
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -183,8 +230,10 @@ public class adminpage extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JButton raderaalien;
     private javax.swing.JTextField txtagent;
     private javax.swing.JTextField txtalien;
+    private javax.swing.JTextField txtutrustning;
     // End of variables declaration//GEN-END:variables
 }
